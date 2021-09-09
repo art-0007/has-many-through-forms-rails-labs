840287ce78521d3c676b51f5acaed9dd9ace37e2
Artur <info@aigroupusa.com>
2021-09-09 16:12:56 -0400

    commit tree f4d1a23cd1f3cef8cbda8cbbbfe3b3a2467cab8e

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,28 @@
 class CommentsController < ApplicationController
 
+  def index
+    @comments = Comment.all 
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new(comment_params) 
+  end
+
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    if params[:comment][:user_id].empty?
+      #binding.pry
+      @user = User.new(username: params[:comment][:user_attributes][:username])
+      @user.save
+    else
+      @user = User.find(params[:comment][:user_id])
+    end
+    @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id])
+
+    redirect_to @comment.post
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,52 @@
 class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
-  end
-
+  before_action :set_post!, only: %i[show destroy edit update]
+ 
   def index
     @posts = Post.all
   end
 
+  def show 
+  end
+
   def new
     @post = Post.new
+    2.times do
+    @post.categories.build
+    end
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
+    @post = Post.create(post_params)
+    if @post.save
+    redirect_to @post
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "Post deleted!"
+    redirect_to posts_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update post_params
+      flash[:success] = "Post updated!"
+      redirect_to post_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+  end
+
+  def set_post!
+    @post = Post.find(params[:id])
   end
 end
